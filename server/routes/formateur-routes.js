9726ffb793b820d7b61b45a0c961f3da481dea7f
unknown <arekik330@gmail.com>
2025-06-05 00:39:04 +0200

    gg

--- server/routes/formateur-routes.js
+++
@@ -1,9 +0,0 @@
-const { createFormateur, getAllFormateurs, getFormateurById, updateFormateur, deleteFormateur } = require('../controllers/formateur-controller');
-
-module.exports = (app) => {
-    app.post('/api/formateur', createFormateur);
-    app.get('/api/formateurs', getAllFormateurs);
-    app.get('/api/formateur/:id', getFormateurById);
-    app.put('/api/formateur/:id', updateFormateur);
-    app.delete('/api/formateur/:id', deleteFormateur);
-};
